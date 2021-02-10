132787b70efb880199c0fc9a042e49367adf7148
SnowCrash <victor.develops@gmail.com>
2021-02-10 20:21:30 +0100

    details page bugs fixed

--- src/app/commons/bands.json.ts
+++ src/app/commons/bands.json.ts
@@ -6,55 +6,65 @@ export const BANDS: Band[]  = [
       formedIn: 1962,
       active: true,
       videoLink: "https://www.youtube.com/watch?v=nrIPxlFzDi0",
-      imgLink: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fclassicsdujour.com%2Fwp-content%2Fuploads%2F2019%2F12%2Frolling-stones-2015-Jim-Pietryga-cc-by-3.0-1200x800.jpg&f=1&nofb=1"
+      imgLink: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fclassicsdujour.com%2Fwp-content%2Fuploads%2F2019%2F12%2Frolling-stones-2015-Jim-Pietryga-cc-by-3.0-1200x800.jpg&f=1&nofb=1",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend sapien eros, volutpat imperdiet neque fringilla sit amet. Morbi vel risus eu diam posuere molestie eget in felis. Curabitur rhoncus aliquet ultricies. Aenean aliquet quis magna non congue. Duis ut nisi nibh. Fusce efficitur vitae mi eget vestibulum. Suspendisse eu neque ullamcorper, fermentum turpis sed, iaculis massa. Donec ut aliquet felis. Ut dignissim, lorem eu elementum finibus, lacus magna semper enim, in aliquet dolor odio sit amet augue."
     },
     {
       name: 'Led Zeppelin',
       formedIn: 1968,
       active: false,
       videoLink: "https://www.youtube.com/watch?v=QkF3oxziUI4",
-      imgLink: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia3.s-nbcnews.com%2Fj%2Fnewscms%2F2016_25%2F1589386%2F160622-led-zeppelin-0545_914bb371ef9d951bd5c79b27cd337962.nbcnews-fp-1200-800.jpg&f=1&nofb=1"
+      imgLink: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia3.s-nbcnews.com%2Fj%2Fnewscms%2F2016_25%2F1589386%2F160622-led-zeppelin-0545_914bb371ef9d951bd5c79b27cd337962.nbcnews-fp-1200-800.jpg&f=1&nofb=1",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend sapien eros, volutpat imperdiet neque fringilla sit amet. Morbi vel risus eu diam posuere molestie eget in felis. Curabitur rhoncus aliquet ultricies. Aenean aliquet quis magna non congue. Duis ut nisi nibh. Fusce efficitur vitae mi eget vestibulum. Suspendisse eu neque ullamcorper, fermentum turpis sed, iaculis massa. Donec ut aliquet felis. Ut dignissim, lorem eu elementum finibus, lacus magna semper enim, in aliquet dolor odio sit amet augue."
     },
     {
       name: 'AC/DC',
       formedIn: 1973,
       active: false,
       videoLink: "https://www.youtube.com/watch?v=v2AC41dglnM",
-      imgLink: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.noise11.com%2Fwp%2Fwp-content%2Fuploads%2F2015%2F12%2FACDC-Melbourne-Concert-151206-12.jpg&f=1&nofb=1"
+      imgLink: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.noise11.com%2Fwp%2Fwp-content%2Fuploads%2F2015%2F12%2FACDC-Melbourne-Concert-151206-12.jpg&f=1&nofb=1",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend sapien eros, volutpat imperdiet neque fringilla sit amet. Morbi vel risus eu diam posuere molestie eget in felis. Curabitur rhoncus aliquet ultricies. Aenean aliquet quis magna non congue. Duis ut nisi nibh. Fusce efficitur vitae mi eget vestibulum. Suspendisse eu neque ullamcorper, fermentum turpis sed, iaculis massa. Donec ut aliquet felis. Ut dignissim, lorem eu elementum finibus, lacus magna semper enim, in aliquet dolor odio sit amet augue."
     },
     {
       name: 'Kiss',
       formedIn: 1973,
       active: false,
       videoLink: "https://www.youtube.com/watch?v=1dFByQyA49I",
-      imgLink: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.AzZ3_3jlulx0M0DThrOiDAHaE8%26pid%3DApi&f=1"
+      imgLink: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.AzZ3_3jlulx0M0DThrOiDAHaE8%26pid%3DApi&f=1",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend sapien eros, volutpat imperdiet neque fringilla sit amet. Morbi vel risus eu diam posuere molestie eget in felis. Curabitur rhoncus aliquet ultricies. Aenean aliquet quis magna non congue. Duis ut nisi nibh. Fusce efficitur vitae mi eget vestibulum. Suspendisse eu neque ullamcorper, fermentum turpis sed, iaculis massa. Donec ut aliquet felis. Ut dignissim, lorem eu elementum finibus, lacus magna semper enim, in aliquet dolor odio sit amet augue."
     },
     {
       name: 'The Ramones',
       formedIn: 1974,
       active: false,
       videoLink: "https://www.youtube.com/watch?v=1WHy8XlwHWk",
-      imgLink: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.vox-cdn.com%2Fthumbor%2Fd2_McSmL0MjAlc7Zqgu9-OXm-PE%3D%2F0x0%3A1200x630%2F1200x800%2Ffilters%3Afocal(504x219%3A696x411)%2Fcdn.vox-cdn.com%2Fuploads%2Fchorus_image%2Fimage%2F63601773%2F4cag_photobydahliakatz_3899_e1553009041565.0.jpg&f=1&nofb=1"
+      imgLink: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.vox-cdn.com%2Fthumbor%2Fd2_McSmL0MjAlc7Zqgu9-OXm-PE%3D%2F0x0%3A1200x630%2F1200x800%2Ffilters%3Afocal(504x219%3A696x411)%2Fcdn.vox-cdn.com%2Fuploads%2Fchorus_image%2Fimage%2F63601773%2F4cag_photobydahliakatz_3899_e1553009041565.0.jpg&f=1&nofb=1",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend sapien eros, volutpat imperdiet neque fringilla sit amet. Morbi vel risus eu diam posuere molestie eget in felis. Curabitur rhoncus aliquet ultricies. Aenean aliquet quis magna non congue. Duis ut nisi nibh. Fusce efficitur vitae mi eget vestibulum. Suspendisse eu neque ullamcorper, fermentum turpis sed, iaculis massa. Donec ut aliquet felis. Ut dignissim, lorem eu elementum finibus, lacus magna semper enim, in aliquet dolor odio sit amet augue."
     },
     {
       name: 'Deep Purple',
       formedIn: 1968,
       active: true,
       videoLink: "https://www.youtube.com/watch?v=ikGyZh0VbPQ",
-      imgLink: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.myeventradar.de%2Fweb%2Fwp-content%2Fuploads%2F2020%2F06%2Fblog-eintrag-2020-deep-purple-1200x800.jpg&f=1&nofb=1"
+      imgLink: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.myeventradar.de%2Fweb%2Fwp-content%2Fuploads%2F2020%2F06%2Fblog-eintrag-2020-deep-purple-1200x800.jpg&f=1&nofb=1",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend sapien eros, volutpat imperdiet neque fringilla sit amet. Morbi vel risus eu diam posuere molestie eget in felis. Curabitur rhoncus aliquet ultricies. Aenean aliquet quis magna non congue. Duis ut nisi nibh. Fusce efficitur vitae mi eget vestibulum. Suspendisse eu neque ullamcorper, fermentum turpis sed, iaculis massa. Donec ut aliquet felis. Ut dignissim, lorem eu elementum finibus, lacus magna semper enim, in aliquet dolor odio sit amet augue."
     },
     {
       name: 'Pink Floyd',
       formedIn: 1965,
       active: false,
       videoLink: "https://www.youtube.com/watch?v=YR5ApYxkU-U",
-      imgLink: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.wallpapersafari.com%2F56%2F39%2FMrJFpd.jpg&f=1&nofb=1"
+      imgLink: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.wallpapersafari.com%2F56%2F39%2FMrJFpd.jpg&f=1&nofb=1",
+      
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend sapien eros, volutpat imperdiet neque fringilla sit amet. Morbi vel risus eu diam posuere molestie eget in felis. Curabitur rhoncus aliquet ultricies. Aenean aliquet quis magna non congue. Duis ut nisi nibh. Fusce efficitur vitae mi eget vestibulum. Suspendisse eu neque ullamcorper, fermentum turpis sed, iaculis massa. Donec ut aliquet felis. Ut dignissim, lorem eu elementum finibus, lacus magna semper enim, in aliquet dolor odio sit amet augue."
     },
     {
       name: 'Queen',
       formedIn: 1970,
       active: true,
       videoLink: "https://www.youtube.com/watch?v=-tJYN-eG1zk",
-      imgLink: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fs1.1zoom.me%2Fbig0%2F36%2F387754-sepik.jpg&f=1&nofb=1"
+      imgLink: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fs1.1zoom.me%2Fbig0%2F36%2F387754-sepik.jpg&f=1&nofb=1",
+      
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend sapien eros, volutpat imperdiet neque fringilla sit amet. Morbi vel risus eu diam posuere molestie eget in felis. Curabitur rhoncus aliquet ultricies. Aenean aliquet quis magna non congue. Duis ut nisi nibh. Fusce efficitur vitae mi eget vestibulum. Suspendisse eu neque ullamcorper, fermentum turpis sed, iaculis massa. Donec ut aliquet felis. Ut dignissim, lorem eu elementum finibus, lacus magna semper enim, in aliquet dolor odio sit amet augue."
     },
   ];
--- src/app/components/band-list/add/add.component.ts
+++ src/app/components/band-list/add/add.component.ts
@@ -23,6 +23,11 @@ export class AddComponent implements OnInit {
       this.band.imgLink =
         'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flibrary.ucf.edu%2Fwp-content%2Fuploads%2Fsites%2F5%2F2015%2F08%2Fphoto-not-available.jpg&f=1&nofb=1';
     }
+
+    if (!this.band.description) {
+      this.band.description =
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend sapien eros, volutpat imperdiet neque fringilla sit amet. Morbi vel risus eu diam posuere molestie eget in felis. Curabitur rhoncus aliquet ultricies. Aenean aliquet quis magna non congue. Duis ut nisi nibh. Fusce efficitur vitae mi eget vestibulum. Suspendisse eu neque ullamcorper, fermentum turpis sed, iaculis massa. Donec ut aliquet felis. Ut dignissim, lorem eu elementum finibus, lacus magna semper enim, in aliquet dolor odio sit amet augue.';
+    }
     this.dialogRef.close(this.band);
   }
 
