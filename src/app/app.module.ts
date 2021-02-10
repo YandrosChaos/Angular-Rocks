fc75bb8f3adaeee0dcb6dfa40bc361f7d6b796e1
SnowCrash <victor.develops@gmail.com>
2021-02-10 13:38:04 +0100

    angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { BandListComponent } from './components/band-list/band-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DetailsComponent } from './components/band-list/details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    BandListComponent
+    BandListComponent,
+    DetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
